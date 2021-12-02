b97ab00c13705c57dd3601a74c6a88f90e323ead
Eric Garnick <eric.b.garnick@gmail.com>
2021-12-01 17:18:32 -0800

    Add file delete functionality

--- FileSystemCrawler/main.go
+++ FileSystemCrawler/main.go
@@ -16,6 +16,8 @@ type config struct {
 	size int64
 	// list files
 	list bool
+	// delete files
+	del bool
 }
 
 func main() {
@@ -23,6 +25,7 @@ func main() {
 	root := flag.String("root", ".", "Root directory to start")
 	// Action options
 	list := flag.Bool("list", false, "List files only")
+	del := flag.Bool("del", false, "Delete files")
 	// Filter options
 	ext := flag.String("ext", "", "File extension to filter out")
 	size := flag.Int64("size", 0, "minimum file size")
@@ -32,6 +35,7 @@ func main() {
 		ext:  *ext,
 		size: *size,
 		list: *list,
+		del:  *del,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
@@ -52,6 +56,10 @@ func run(root string, out io.Writer, cfg config) error {
 			// If list was explicitly set, don't do anything else
 			if cfg.list {
 				return listFile(path, out)
+			}
+			// Delete files
+			if cfg.del {
+				return delFile(path)
 			}
 			// List is the default option if nothing else was set
 			return listFile(path, out)
